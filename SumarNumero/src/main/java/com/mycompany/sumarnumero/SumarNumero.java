fa0d4c438eaa088ce9641c95b5b2ee16c5c82867
apmbgb <angiem0118b@gmail.com>
2024-08-05 21:15:52 -0500

    División de dos numeros

--- SumarNumero/src/main/java/com/mycompany/sumarnumero/SumarNumero.java
+++ SumarNumero/src/main/java/com/mycompany/sumarnumero/SumarNumero.java
@@ -1,9 +1,13 @@
 package com.mycompany.sumarnumero;
-
+import java.util.Scanner;
 public class SumarNumero {
 
     public static void main(String[] args) {
-        
-        System.out.println("Hello World!");
+        Scanner sc = new Scanner (System.in);
+        int numero1, numero2;
+        numero1 = sc.nextInt();
+        numero2 = sc.nextInt();
+        int resultado = numero2/numero2;
+        System.out.println("La dision entre los numeros es "+numero2+"/"+numero1+" "+resultado );
     }
 }
